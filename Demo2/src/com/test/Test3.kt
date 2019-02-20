12707f8e67fe100faf97ca446611d44511992510
525688067@qq.com <lzl19861009>
2019-02-20 22:42:07 +0800

    kotlin

--- Demo2/src/com/test/Test3.kt
+++ Demo2/src/com/test/Test3.kt
@@ -5,8 +5,8 @@ package com.test
  *
  * 这里size是提供给对外的变量，_size是内部使用的，其实最后还是通过函数来控制的
  * 这里_size就叫做幕后属性
-其实幕后属性就是我们设计类属性时的一种实现方法而已
-不同于field幕后字段是Kotlin内置提供给我们的
+ 其实幕后属性就是我们设计类属性时的一种实现方法而已
+ 不同于field幕后字段是Kotlin内置提供给我们的
  */
 class Demo {
     val size get() = _size
---
+++ Demo2/src/com/test/Test4.kt
@@ -0,0 +1,18 @@
+package com.test
+
+
+class Foo {
+    lateinit var   lateinitVar: String
+
+    fun initializationLogic() {
+        //sampleStart
+        println("isInitialized before assignment: " + this::lateinitVar.isInitialized)
+        lateinitVar = "value"
+        println("isInitialized after assignment: " + this::lateinitVar.isInitialized)
+        //sampleEnd
+    }
+}
+
+fun main(args: Array<String>) {
+    Foo().initializationLogic()
+}
